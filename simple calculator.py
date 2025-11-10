2e7a34e06a78f6520e02e56fc7619609df2a5212
Naveen <naveensainipc876@gmail.com>
2025-11-10 15:07:18 +0530

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,16 @@
+
+sum
+a=5
+b=6
+choice=input("enter your choice +-*/")
+
+if choice=="+":
+     print(a+b)
+elif choice=="-":
+     print(a+b)
+elif choice=="*":
+     print(a*b)
+elif choice=="/":
+     print (a/b)
+else:
+         print("invalid choice")
